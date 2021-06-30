ea1fd5148273d51383122f04db4b7880019cdf4e
Monkefy <brambartels200@gmail.com>
2021-06-30 13:42:59 +0200

    Finished the SQLite class.

--- src/main/java/xyz/monkefy/Levels.java
+++ src/main/java/xyz/monkefy/Levels.java
@@ -6,10 +6,13 @@ import java.util.logging.Logger;
 
 public final class Levels extends JavaPlugin {
 
+    private static String prefix;
+
     private static Levels a;
 
     @Override
     public void onEnable() {
+        prefix = this.getConfig().getString("plugin-prefix");
         a = this;
         Logger logger = this.getLogger();
         logger.info("Enabling plugin.");
@@ -23,5 +26,9 @@ public final class Levels extends JavaPlugin {
 
     public static Levels getInstance() {
         return a;
+    }
+
+    public static String getPrefix() {
+        return prefix + " ";
     }
 }
--- src/main/java/xyz/monkefy/database/SQLite.java
+++ src/main/java/xyz/monkefy/database/SQLite.java
@@ -1,11 +1,14 @@
 package xyz.monkefy.database;
 
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import xyz.monkefy.Levels;
 import xyz.monkefy.utilities.StackUtil;
 
 import java.io.File;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.Stack;
 
 public class SQLite implements DataManager{
 
@@ -14,6 +17,7 @@ public class SQLite implements DataManager{
     private String fileDirectory;
     private String fileName;
     Table table;
+    String prefix = Levels.getPrefix();
 
     public SQLite(String fileDirectory, String fileName) {
         this.fileDirectory = fileDirectory;
@@ -27,50 +31,132 @@ public class SQLite implements DataManager{
             } catch (IOException e) {
                 StackUtil.dumpStack(e);
             }
+        open();
     }
+
+    protected SQLite(FileConfiguration fileConfiguration) {}
 
     @Override
     public void setTable(Table table) {
-
+        this.table = table;
+        try {
+            this.statement.executeUpdate("CREATE TABLE IF NOT EXISTS " + table.getName() + table.getUsage());
+        } catch (SQLException e) {
+            Bukkit.getLogger().severe(prefix + "Couldn't create SQL task, plugin won't work without it.");
+            StackUtil.dumpStack(e);
+            return;
+        }
     }
 
     @Override
     public void set(Object... objectParam) {
+        try {
+            String value = "";
+            for(int i = 0; i < objectParam.length; i++) {
+                value = String.valueOf(objectParam) + "?";
+                if(i < objectParam.length - 1) value = String.valueOf(value) + "?";
+            }
 
+            PreparedStatement preparedStatement = this.con.prepareStatement("INSERT INTO " + this.table.getName() + this.table.getValues() + " VALUES(" + objectParam + ");");
+            for(int j = 0; j < objectParam.length; j++) preparedStatement.setObject( j + 1, objectParam[j]);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            Bukkit.getLogger().severe(prefix + "Failed SQL task.");
+            StackUtil.dumpStack(e);
+            return;
+        }
     }
 
     @Override
     public Object get(String parameter1, String parameter2, Object objParam) {
+        try {
+            PreparedStatement preparedStatement = this.con.prepareStatement("SELECT * FROM " + this.table.getName() + " WHERE " + parameter1 + "=?;");
+            preparedStatement.setObject(1, objParam);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if(resultSet.next()) return resultSet.getObject(parameter2);
+        } catch (SQLException e) {
+            Bukkit.getLogger().severe(prefix + "Failed SQL task.");
+            StackUtil.dumpStack(e);
+        }
         return null;
     }
 
     @Override
     public boolean contains(String parameterString, Object parameterObject) {
-        return false;
+        try {
+            PreparedStatement preparedStatement = this.con.prepareStatement("SELECT * FROM " + this.table.getName() + " WHERE " + parameterString + "=?;");
+            preparedStatement.setObject(1, parameterObject);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            return resultSet.next();
+        } catch (SQLException e) {
+            Bukkit.getLogger().severe(prefix + "Failed SQL task.");
+            StackUtil.dumpStack(e);
+            return false;
+        }
     }
 
     @Override
     public void update(String parameter1, String parameter2, Object paramObj1, Object paramObj2) {
-
+        try {
+            PreparedStatement preparedStatement = this.con.prepareStatement("UPDATE " + this.table.getName() + " SET " + parameter2 + "=? WHERE " + parameter1 + "=?;");
+            preparedStatement.setObject(1, paramObj2);
+            preparedStatement.setObject(2, paramObj1);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            Bukkit.getLogger().severe(prefix + "Failed SQL task.");
+            StackUtil.dumpStack(e);
+            return;
+        }
     }
 
     @Override
     public void remove(String paramString1, Object objParam) {
-
+        try {
+            PreparedStatement preparedStatement = this.con.prepareStatement("DELETE FROM " + this.table.getName() + " WHERE " + paramString1 + "=?;");
+            preparedStatement.setObject(1, objParam);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            Bukkit.getLogger().severe(prefix + "Failed SQL task.");
+            StackUtil.dumpStack(e);
+            return;
+        }
     }
 
     @Override
     public boolean isOpen() {
-        return false;
+        return !(this.con == null && this.statement == null);
     }
 
     @Override
     public void open() {
-
+        try {
+            Class.forName("org.sqlite.JDBC");
+        } catch(ClassNotFoundException e) {
+            Bukkit.getLogger().severe(prefix + "Failed to open SQLite driver.");
+            StackUtil.dumpStack(e);
+            return;
+        }
+        try {
+            this.con = DriverManager.getConnection("jdbc:sqlite" + this.fileDirectory + File.separator + this.fileName);
+            this.statement = this.con.createStatement();
+            this.statement.setQueryTimeout(30);
+        } catch (SQLException e) {
+            Bukkit.getLogger().severe(prefix + "Failed to open SQLite connection");
+            StackUtil.dumpStack(e);
+            return;
+        }
     }
 
     @Override
     public void close() {
-
+        try {
+            if(this.statement != null)
+                this.statement.close();
+            if(this.con != null)
+                this.con.close();
+            } catch (SQLException e) {
+            Bukkit.getLogger().severe(prefix + "Failed to close SQL");
+            StackUtil.dumpStack(e);
+        }
     }
 }
